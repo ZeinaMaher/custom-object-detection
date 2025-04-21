662a9e44049d7f20d095ca7430bb5af225ce07c5
ZeinaMaher <zmzeina0@gmail.com>
2025-04-21 10:37:43 +0300

    Add load model function

--- utils/utils.py
+++ utils/utils.py
@@ -1,6 +1,11 @@
 import os
+import sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 import torch
 import torch.nn.functional as F
+from model.arch import ModelBuilder
+
 
 def create_target_map(targets, grid_size, num_classes, device):
     """
@@ -61,18 +66,51 @@ def save_checkpoint(model, optimizer, epoch, loss, path):
     torch.save(checkpoint, path)
     print(f"Checkpoint saved at: {path}")
 
+def load_model_from_checkpoint(config_path, checkpoint_path, device=None):
+    """
+    Loads a model and its weights from a checkpoint file.
+
+    Args:
+        config_path (str): Path to the model config YAML.
+        checkpoint_path (str): Path to the saved checkpoint file.
+        device (torch.device or str): Target device ('cpu' or 'cuda').
+
+    Returns:
+        model (nn.Module): Model with loaded weights.
+        checkpoint (dict): Full checkpoint dictionary.
+    """
+    if device is None:
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    # Step 1: Rebuild model architecture
+    model = ModelBuilder.from_config(config_path)
+    model.to(device)
+
+    # Step 2: Load checkpoint and apply weights
+    checkpoint = torch.load(checkpoint_path, map_location=device)
+    model.load_state_dict(checkpoint["model_state_dict"])
+
+    print(f"=Model loaded from {checkpoint_path} onto {device}")
+    return model, checkpoint
+
 
 if __name__ == "__main__":
-    targets = (
-        torch.tensor([[3.0, 0.1320, 0.2031, 0.0805, 0.1133],  # Object 1
-                    [3.0, 0.3133, 0.0984, 0.0961, 0.1297],  # Object 2
-                    [3.0, 0.5672, 0.1586, 0.0672, 0.1297]]),  # Object 3
-        torch.tensor([[3.0, 0.0781, 0.3844, 0.0562, 0.0852],  # Object 1
-                    [3.0, 0.1977, 0.3750, 0.0477, 0.0828],  # Object 2
-                    [8.0, 0.5672, 0.3758, 0.0383, 0.0750]]),  # Object 3
-    )
 
-    target_map = create_target_map(targets, grid_size=(30, 30), num_classes=2, device='cpu')
+    #to test target map 
+    # targets = (
+    #     torch.tensor([[3.0, 0.1320, 0.2031, 0.0805, 0.1133],  # Object 1
+    #                 [3.0, 0.3133, 0.0984, 0.0961, 0.1297],  # Object 2
+    #                 [3.0, 0.5672, 0.1586, 0.0672, 0.1297]]),  # Object 3
+    #     torch.tensor([[3.0, 0.0781, 0.3844, 0.0562, 0.0852],  # Object 1
+    #                 [3.0, 0.1977, 0.3750, 0.0477, 0.0828],  # Object 2
+    #                 [8.0, 0.5672, 0.3758, 0.0383, 0.0750]]),  # Object 3
+    # )
 
-    print(target_map.shape)  # Should print: torch.Size([2, 7, 30, 30]) for a batch of size 2
+    # target_map = create_target_map(targets, grid_size=(30, 30), num_classes=2, device='cpu')
 
+    # print(target_map.shape)  # Should print: torch.Size([2, 7, 30, 30]) for a batch of size 2
+
+    # test model loading 
+
+    model, checkpoint= load_model_from_checkpoint('./configs/simple_model.yaml', './model_epoch_1.pth')
+    print(model, checkpoint)
