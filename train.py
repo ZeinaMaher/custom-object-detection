e4422835cb8ce47d8507e160ce3f02f5d140c899
ZeinaMaher <zmzeina0@gmail.com>
2025-04-21 17:05:15 +0300

    save models based on validation result

--- train.py
+++ train.py
@@ -34,21 +34,59 @@ def train_one_epoch(model, dataloader, optimizer, criterion, device):
 
     return epoch_loss / len(dataloader)
 
+def validate_one_epoch(model, dataloader, criterion, device):
+    model.eval()
+    val_loss = 0.0
+
+    with torch.no_grad():
+        for images, targets, _ in dataloader:
+            images = images.to(device)
+            preds = model(images)
+
+            _, _, H, W = preds.shape
+            target_map = create_target_map(
+                targets,
+                grid_size=(H, W),
+                num_classes=model.head.num_classes,
+                device=device
+            )
+
+            loss = criterion(preds, target_map)
+            val_loss += loss.item()
+
+    return val_loss / len(dataloader)
+
 def train(model_config, data_config, criterion, optimizer ,device, num_epochs, lr, batch_size, file_path=None ):
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     model = ModelBuilder.from_config("./configs/simple_model.yaml").to(device)
 
-    dataset = CustomDataset('./dataset/filtered_data/train/images', './dataset/filtered_data/train/labels', shuffle=True, normalize=True)
-    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True, collate_fn=collate_fn)
+    train_dataset = CustomDataset('./dataset/filtered_data/train/images', './dataset/filtered_data/train/labels', shuffle=True, normalize=True)
+    val_dataset = CustomDataset('./dataset/filtered_data/valid/images',
+                                './dataset/filtered_data/valid/labels',
+                                shuffle=False, normalize=True)
+
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, collate_fn=collate_fn)
+    val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False, collate_fn=collate_fn) 
 
     optimizer = torch.optim.Adam(model.parameters(), lr=lr)
+
+    best_val_loss = float('inf')
+
     # Training loop
     for epoch in tqdm(range(num_epochs)):
-        loss= train_one_epoch(model,dataloader, optimizer, criterion, device)
+        train_loss= train_one_epoch(model,train_loader, optimizer, criterion, device)
 
-        print(f"Epoch [{epoch+1}/{num_epochs}], Loss: {loss:.4f}")
-        save_checkpoint(model, optimizer, epoch,loss, 'checkpoints/model_epoch_{epoch+1}.pth')
+        val_loss = validate_one_epoch(model, val_loader, criterion, device)
+        print(f"Epoch [{epoch+1}/{num_epochs}] | Train Loss: {train_loss:.4f} | Val Loss: {val_loss:.4f}")
+
+        if epoch == 0:
+            save_checkpoint(model, optimizer, epoch,val_loss, 'checkpoints/model_epoch_{epoch+1}.pth')
+
+        if val_loss < best_val_loss:
+            best_val_loss = val_loss
+            save_checkpoint(model, optimizer, epoch, val_loss, 'checkpoints/best_model.pth')
+            print(f"Best model saved at epoch {epoch+1} with val loss {val_loss:.4f}")
 
 
 if __name__ == "__main__":
