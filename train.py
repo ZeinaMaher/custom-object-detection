3255ebd1fad882ab2fb93cc96bd02891cb1e9bfc
ZeinaMaher <zmzeina0@gmail.com>
2025-04-22 19:24:20 +0300

    Add scheduler to update learning rate and add int_weight fun

--- train.py
+++ train.py
@@ -1,12 +1,28 @@
 
 import torch
 from tqdm import tqdm
+import torch.nn as nn
 from torch.utils.data import DataLoader
 from model.arch import ModelBuilder
 from model.loss import DetectionLoss
 from data.loader import CustomDataset, collate_fn
 from data.augment import CustomAugmentations
 from utils.utils import create_target_map , save_checkpoint
+
+def init_weights(m):
+    """Initialize only unfrozen layers"""
+    if not hasattr(m, 'weight') or not m.weight.requires_grad:
+        return  # Skip frozen layers
+    
+    if isinstance(m, nn.Conv2d):
+        nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+        if m.bias is not None:
+            nn.init.constant_(m.bias, 0)
+    elif isinstance(m, nn.BatchNorm2d):
+        if m.weight.requires_grad:  # Only init trainable BatchNorm
+            nn.init.constant_(m.weight, 1)
+            nn.init.constant_(m.bias, 0)
+
 
 def train_one_epoch(model, dataloader, optimizer, criterion, device):
     model.train()
@@ -26,6 +42,7 @@ def train_one_epoch(model, dataloader, optimizer, criterion, device):
             device= device
         )
         loss = criterion(preds, target_map) # make sure from the shape
+        print("Loss components (xy, wh, obj, cls):", loss.item())
 
         optimizer.zero_grad()
         loss.backward()
@@ -61,19 +78,27 @@ def train(model_config, data_config, criterion, optimizer ,device, num_epochs, l
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     model = ModelBuilder.from_config("./configs/model_v1.yaml").to(device)
+    # model.apply(init_weights)
 
-    augmentations = CustomAugmentations(p_flip=1.0)
-    train_dataset = CustomDataset('./dataset/filtered_data/train/images',
-                                  './dataset/filtered_data/train/labels',
+    augmentations = CustomAugmentations(p_flip=0.5)
+    # train_dataset = CustomDataset('./dataset/filtered_data/train/images',
+    #                               './dataset/filtered_data/train/labels',
+    #                                shuffle=True, normalize=True, augmentations=augmentations)
+    # val_dataset = CustomDataset('./dataset/filtered_data/valid/images',
+    #                             './dataset/filtered_data/valid/labels',
+    #                             shuffle=False, normalize=True)
+    train_dataset = CustomDataset('./dataset/samples/valid/images',
+                                  './dataset/samples/valid/labels',
                                    shuffle=True, normalize=True, augmentations=augmentations)
-    val_dataset = CustomDataset('./dataset/filtered_data/valid/images',
-                                './dataset/filtered_data/valid/labels',
+    val_dataset = CustomDataset('./dataset/samples/valid/images',
+                                './dataset/samples/valid/labels',
                                 shuffle=False, normalize=True)
 
     train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, collate_fn=collate_fn)
     val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False, collate_fn=collate_fn) 
 
-    optimizer = torch.optim.Adam(model.parameters(), lr=lr)
+    optimizer = torch.optim.Adam(model.parameters(), lr=lr)  # Start smaller
+    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'min', patience=2)
 
     best_val_loss = float('inf')
 
@@ -92,9 +117,14 @@ def train(model_config, data_config, criterion, optimizer ,device, num_epochs, l
             save_checkpoint(model, optimizer, epoch, val_loss, 'checkpoints/Exp1_best_model.pth')
             print(f"Best model saved at epoch {epoch+1} with val loss {val_loss:.4f}")
 
+        scheduler.step(val_loss)
+        print(f"LR: {optimizer.param_groups[0]['lr']:.2e}")  # Log current LR
+
 
 if __name__ == "__main__":
     criterion = DetectionLoss()
     train(model_config= '.\configs\model_v1.yaml',
           data_config='', criterion=criterion, optimizer=None,
-          device=None,  num_epochs=10, lr = 1e-3, batch_size=8 )
+          device=None,  num_epochs=300, lr = 1e-3, batch_size=1 )
+
+          #1e-4
